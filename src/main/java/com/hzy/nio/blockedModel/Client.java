2ba86283bc0303bff7c4989fe9c168bc0afe0ff5
贺中禹 <974529345@qq.com>
2022-04-04 17:58:55 +0800

    第一次使用非阻塞模式，accept和read方法不会再阻塞了 而是有特定返回值 比如accept没有看到连接 就返回null，read没有可读内容就返回0 出现了点小问题 虽然可以用isOpen判断是否channel还开着，但是channel可以随时断开 断开就会报错

--- src/main/java/com/hzy/nio/blockedModel/Client.java
+++ src/main/java/com/hzy/nio/blockedModel/Client.java
@@ -6,13 +6,30 @@ import java.nio.channels.SocketChannel;
 import java.nio.charset.Charset;
 import java.util.Scanner;
 
-public class Client {
+public class Client implements Runnable{
+    @Override
+    public void run() {
+        SocketChannel sc = null;
+        try {
+            sc = SocketChannel.open();
+            sc.connect(new InetSocketAddress("localhost",8787));
+            Scanner scanner = new Scanner(System.in);
+            while (true){
+                String msg = scanner.nextLine();
+                sc.write(Charset.defaultCharset().encode(msg));
+                if ("bye".equals(msg)){
+                    break;
+                }
+            }
+            System.out.println("waiting.......");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     public static void main(String[] args) throws IOException {
-        SocketChannel sc = SocketChannel.open();
-        sc.connect(new InetSocketAddress("localhost",8787));
-        Scanner scanner = new Scanner(System.in);
-        sc.write(Charset.defaultCharset().encode(scanner.nextLine()));
-        System.out.println("waiting.......");
+        new Thread(new Client()).start();
 
     }
 }
--- src/main/java/com/hzy/nio/blockedModel/Server.java
+++ src/main/java/com/hzy/nio/blockedModel/Server.java
@@ -22,6 +22,9 @@ public class Server {
 
         //创建一个ServerSocket
         ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        //设置为非阻塞状态
+        //如果设置成了非阻塞效果，那么本应该阻塞的accept和read都不会阻塞，但是accept返回的SocketChannel就是null了
+        serverSocketChannel.configureBlocking(false);
 
         //给ServerSocket绑定一个端口
         serverSocketChannel.bind(new InetSocketAddress(8787));
@@ -31,20 +34,30 @@ public class Server {
 
         //下面的accept和read方法都是阻塞的，所以当同时有多个连接的建立时，一个连接的阻塞会影响到其他的连接不能继续执行
         while(true){
-            //阻塞式建立连接
-            System.out.println("正在连接中");
             //accept() 和传统Socket一样是阻塞的 如果没有建立连接 代码就不会往下走 会一直堵在accept这一行
             SocketChannel sc = serverSocketChannel.accept();
-            System.out.println("连接成功" + sc.getRemoteAddress());
-            channels.add(sc);
+            if (sc != null){
+                System.out.println("连接成功" + sc.getRemoteAddress());
+                channels.add(sc);
+                //要把获得的socketChannel也设置成非阻塞的，才能保证read方法也不阻塞
+                sc.configureBlocking(false);
+            }
             for (SocketChannel sc1:channels){
+                if (!sc1.isOpen()){
+                    //循环删除已经断开连接的SocketChannel
+                    channels.remove(sc1);
+                    continue;
+                }
                 //接收客户端发来的消息 写到buffer里去
                 //同时这个read方法也是阻塞的，在对方没有发来消息之前，下面的代码也不会执行
-                sc1.read(buffer);
-                System.out.println(new String(buffer.array()));
-                //调转读写模式
-                buffer.flip();
-                buffer.clear();
+                int code = sc1.read(buffer);
+                if (code > 0){
+                    //如果没有读取到内容 那么read返回值就是0
+                    System.out.println(buffer.toString());
+                    //调转读写模式
+                    buffer.flip();
+                    buffer.clear();
+                }
             }
         }
     }
